2afa0f075b00353c373890d7112032e4b6d34308
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-02 13:14:21 +0530

    Added test.js for class-4

---
+++ JavaScript/class-4/test.js
@@ -0,0 +1,52 @@
+// ✅ Git Questions
+
+// 1. What command initializes a new Git repository in your project folder?
+
+// 2. How to check the current status of your Git repository?
+
+// 3. Write the command to add all changes to the staging area.
+
+// 4. Commit your staged changes with the message: 'First commit'.
+
+// 5. Clone a GitHub repository using its URL. (URL: git@github.com:therahulkumar9/R-Tech.git)
+
+// 6. Write the command to view the commit history of your project.
+
+
+// 💻 JavaScript – Code-Based Questions
+
+// 1. Change Text of a DOM Element
+// Q: Select the element with ID "title" and change its text to "Welcome".
+
+// 2. Select Element & Change Style
+// Q: Select an element with the ID "box" and change its background color black to blue.
+
+// 3. Generate Random Number
+// Q: Generate a random number between 1 and 10 and display it using alert().
+
+// 4. Even or Odd Function
+// Q: Write a function to check if a number from an input field is even or odd.
+
+// 5. Using setInterval()
+// Q: Log "Running..." every second.
+
+// 6. Using slice()
+// Q: Extract the first 4 characters from "JavaScript".
+
+// 7. Object Properties
+// Q: Create a user object and log the name property.
+
+// 8. Add Property to Object
+// Q: Add a new key isLoggedIn: true to the user object and print it.
+
+// 9. Change Image on Click
+// Q: Change the src of an image to "new.png" when it’s clicked.
+
+// 10. Create a Countdown Timer
+// Q: Display a countdown from 5 to 0 using setInterval().
+
+// 11. Filter Even Numbers
+// Q: Filter even numbers from the array [1, 2, 3, 4, 5].
+
+// 12. Uppercase Conversion
+// Q: Convert ["apple", "banana"] to uppercase.
