3e40a6063188e7c1022d906c040437f079cc348a
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-03 16:43:12 +0530

    Added test answers

---
+++ JavaScript/class-4/answers.js
@@ -0,0 +1,21 @@
+// Answers
+
+// 1
+console.log("git init")
+
+// 2
+console.log("git status")
+
+// 3
+console.log("git add .")
+
+// 4
+console.log(`git commit -m "First commit"`)
+
+// 5
+console.log("git clone git@github.com:therahulkumar9/R-Tech.git")
+
+// 6
+
+console.log("git log")
+console.log("git log --oneline")
