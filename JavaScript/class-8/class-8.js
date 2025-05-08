a0e5e3da98480c0345539aa8b371156e41a95e5e
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-08 15:44:16 +0530

    Added slice example

---
+++ JavaScript/class-8/class-8.js
@@ -0,0 +1,17 @@
+let collegeName = "Nalanda College";
+let userName = "Mohan Yadav";
+
+
+console.log(collegeName.slice(0, 7));
+console.log(userName.slice(0, 4));
+
+let arr = [3, 9, 23, 12, 23]
+console.log(arr);
+
+let subArr = arr.slice(0, 3)
+
+console.log(subArr);
+console.log(arr);
+
+
+
