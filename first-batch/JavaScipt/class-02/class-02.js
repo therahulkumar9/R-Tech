01d4bf1123924af030205791af3affecca8cf2be
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-20 10:18:08 +0530

    Added class-02.js

---
+++ first-batch/JavaScipt/class-02/class-02.js
@@ -0,0 +1,33 @@
+// Variable Naming Rules
+
+// case sensitive
+let rahul;
+let Rahul;
+let RAHUL;
+let rAHUL;
+
+let rahul1;
+let _rahul;
+let $rahul;
+
+// error
+// let 1rahul; 
+// let @rahul;
+
+
+// Reseverd keyword
+
+// let if = 10;
+// let switch = 10;
+// let for = 20;
+// let function = 40
+
+// Camel Case
+let fruitNames = ['mango', 'banana', 'apple'];
+let userName = "therahulkumar9";
+
+// Pascal Case
+let CompanyName = 'R-Tech';
+
+// Snake Case
+let my_value = 21;
