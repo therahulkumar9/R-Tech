f002eab2bdbd3d054deb15ed6a45e4d122d2aeec
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-21 10:26:25 +0530

    Added code for operator

---
+++ first-batch/JavaScipt/class-03/class-03.js
@@ -0,0 +1,37 @@
+console.log("Operator");
+
+//  Assignment Operators
+var num = 12;
+var userName = "Rahul";
+var isLoggedIn = true;
+
+var isEligible = false;
+console.log(num);
+
+
+// +=
+var score = 10;
+console.log(score);
+// score = score + 2;
+score += 2;
+console.log(score);
+
+// *=
+var score = 10;
+console.log(score);
+// score = score * 3
+score *= 3;
+// score /= 3;
+// score -= 3;
+console.log(score);
+
+// %
+// 8 % 3 is equal to 2
+// var result = 10 % 3;
+var num1 = 8;
+var num2 = 3; 
+console.log(num1);
+num1 %= 3;
+console.log(num1);
+
+
