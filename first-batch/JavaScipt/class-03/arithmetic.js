2cac1a105a7f0256b5b95a6e8b0a9a7c7e363ffb
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-21 10:26:49 +0530

    Added notes for arithmetic operator

---
+++ first-batch/JavaScipt/class-03/arithmetic.js
@@ -0,0 +1,45 @@
+ // Arithmetic Operators
+ 
+ var num1 = 100;
+ var num2 = 2;
+ // 1. Addition
+ var result = num1 + num2; // 102
+ console.log(result);
+ 
+ // 2. Subtraction
+ var result = num1 - num2; // 98
+ console.log(result);
+ 
+ // 3. Multiplication
+ var result = num1 * num2; // 200
+ // 4. Division
+ var result = num1 / num2; // 50
+ // 5. Modulus
+ var result = num1 % num2; // 0
+
+ // 6. Exponential
+ var result = num1 ** num2; // 10000
+ console.log(result);
+ 
+//  num1 ^ 2 -> 100 ^ 2 = 1000
+ 
+ // 7. Pre Increment
+ var num = 10;
+//  num++;
+//  ++num;
+ var result = ++num; // num = 11 ; result = 11
+
+
+ // 8. Post Increment
+ var num = 10;
+ var result = num++; // result = 10 ; num = 11
+
+
+ // 9. Pre Decrement
+ var num = 10;
+ var result = --num; // num = 9 ; result = 9
+
+
+ // 10. Post Decrement
+ var num = 10;
+ var result = num--; // result = 10 ; num = 9
