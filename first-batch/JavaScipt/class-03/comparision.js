2a8398d917f545ab091c2358d7b66370815bca22
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-21 10:27:05 +0530

    Added notes for comparision operator

---
+++ first-batch/JavaScipt/class-03/comparision.js
@@ -0,0 +1,55 @@
+var x = 45;
+var y = 67;
+
+// ==
+var result = x == y; // false
+
+// <
+var result = x < y; // true
+
+
+// >
+var result = y > x // true
+
+
+// >=
+// 21 >= 23 // false
+// console.log(21 >= 23);
+// console.log(21 >= 21);
+// console.log(34 <= 39);
+// console.log(34 <= 34);
+
+
+// ===
+var num1 = 10
+var num2 = 23;
+var num3 = 10;
+
+console.log('Result', num1 === num3);
+
+
+// var result = num1 == num2 // false
+var result = num1 === num3 // true
+// console.log(result);
+
+// !=
+// var result = num1 != num2
+var result = num1 !== num3
+console.log(result);
+
+
+
+// === 
+// true and true = true
+// false and false = false
+// false and true = false;
+// true and false = false;
+
+
+
+
+
+
+
+
+
