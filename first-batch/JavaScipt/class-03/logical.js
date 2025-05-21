d1e4fbf8093f41fe38969d5766ecbc2569c0f86f
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-21 10:27:28 +0530

    Added note for logical operator

---
+++ first-batch/JavaScipt/class-03/logical.js
@@ -0,0 +1,34 @@
+// Logical Operator
+
+// &&
+// true and true = true
+// false and false = false
+// false and true = false;
+// true and false = false;
+var num1 = 23;
+var num2 = 56;
+var num3 = 100;
+
+var result = num1 < num2 && num2 > num3;
+console.log(result);
+
+
+// ||
+
+// true and true = true
+// false and false = false
+// false and true = true;
+// true and false = true;
+
+var num1 = 23;
+var num2 = 56;
+var num3 = 100;
+
+var result = num1 > num2 || num2 < num3;
+console.log(result);
+
+
+// !
+
+// console.log("Result", !(4 > 5));
+console.log("Result", !(64 < 22));
