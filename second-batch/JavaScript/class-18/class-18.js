27b663131c66f5bcd8f3bd7bacdc880c37bcc54e
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-21 16:14:23 +0530

    Added notes for class-18

---
+++ second-batch/JavaScript/class-18/class-18.js
@@ -0,0 +1,27 @@
+// Array handling => map, filter
+// data type 
+// Conditional Statements (if, else if, else, switch)
+// Array Methods (map, filter, reduce, forEach, find)
+// Scope (Global, Local, Block)
+// Loops (for, while, do...while, for...in, for...of)
+// JSON
+// ternary operator 
+// callback
+// function, arrow functon
+// Destructuring (Objects and Arrays)
+// fetch or axios 
+// Logical Operators
+
+let myObject = {
+    name: "Rahul",
+    age: 18,
+    class: 12,
+    greet: function(){
+        console.log("Hello my name is", this.name, "age is", this.age );
+    }
+}
+
+console.log(myObject.greet());
+
+
+
