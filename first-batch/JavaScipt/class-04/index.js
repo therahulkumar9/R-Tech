cdee0e662749a8c516c61fa1c809082f18aeade2
Rahul Kumar <therahulkumar135@gmail.com>
2025-05-22 10:12:50 +0530

    Added code for class-4

---
+++ first-batch/JavaScipt/class-04/index.js
@@ -0,0 +1,45 @@
+// Logical
+
+// &&
+// var condition1 = true;
+// var condition2 = true;
+
+if(condition1 && condition2) {
+    console.log("Block of code to execute");
+}
+
+
+// ||
+var condition1 = true;
+var condition2 = true;
+
+
+if(condition1 || condition2) {
+    console.log("Block of code to execute");
+}
+
+
+// if(true) {
+//     // code
+// }
+
+
+// if else
+
+let isEligible = true;
+
+if(isEligible){
+    console.log("User is eligible.");
+} else {
+    console.log("User is not eligible.");
+}
+
+
+// !
+
+var condition1 = false;
+var newCondition = !condition1;
+
+
+console.log(condition1);
+console.log(newCondition);
